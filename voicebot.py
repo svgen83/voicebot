fad8640ca2d22e1c89aa80ba804bfa35697040ba
svgen83 <61458549+svgen83@users.noreply.github.com>
2022-04-10 16:26:49 +0400

    удален "лишний" код

--- voicebot.py
+++ voicebot.py
@@ -13,7 +13,6 @@ GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 PROJECT_ID = os.getenv("PROJECT_ID")
 
 
-# Enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
@@ -21,8 +20,6 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
 def start(update: Update, context: CallbackContext):
     """Send a message when the command /start is issued."""
     user = update.effective_user
@@ -33,12 +30,10 @@ def start(update: Update, context: CallbackContext):
 
 
 def help_command(update: Update, context: CallbackContext):
-    """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
 
 def echo(update: Update, context: CallbackContext):
-    """Echo the user message."""
     msg = detect_intent_texts(PROJECT_ID, update.message.chat_id, update.message.text, 'ru')
     update.message.reply_text(msg)
     
@@ -49,44 +44,20 @@ def detect_intent_texts(project_id, session_id, text, language_code):
   text_input = dialogflow.TextInput(text=text, language_code=language_code)
   query_input = dialogflow.QueryInput(text=text_input)
   response = session_client.detect_intent(session=session, query_input=query_input)
-  print("=" * 20)
-  print("Query text: {}".format(response.query_result.query_text))
-  print(
-            "Detected intent: {} (confidence: {})\n".format(
-                response.query_result.intent.display_name,
-                response.query_result.intent_detection_confidence,
-            )
-        )
-  print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
   return response.query_result.fulfillment_text
 
 
 
-def main() -> None:
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    #load_dotenv()
-    #TG_TOKEN = os.getenv("TG_TOKEN")
-    
-    
+def main():
     updater = Updater(TG_TOKEN)
-
-    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
-    # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
 
-    # on non command i.e message - echo the message on Telegram
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
