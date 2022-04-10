61226854562c02ec92c407c7df5e817d6886e930
svgen83 <61458549+svgen83@users.noreply.github.com>
2022-04-10 19:14:28 +0400

    добавлен эхобот

--- vk_bot.py
+++ vk_bot.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import random
 import vk_api
 
 from google.cloud import dialogflow
@@ -21,17 +22,13 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def vk_chat(vk_token):
-    vk_session = vk_api.VkApi(token=vk_token)
-    longpoll = VkLongPoll(vk_session)
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1,1000)
+    )
+    print(event.text)
 
 
 if __name__ == '__main__':
@@ -45,13 +42,14 @@ if __name__ == '__main__':
     longpoll = VkLongPoll(vk_session)
     vk_api = vk_session.get_api()
     
-    while True:
-        for event in longpoll.listen():
-            if event.type == VkEventType.MESSAGE_NEW:
-                print('Новое сообщение:')
-                if event.to_me:
-                    print('Для меня от: ', event.user_id)
-                else:
-                    print('От меня для: ', event.user_id)
-                print('Текст:', event.text)
+    #while True:
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
+                #print('Новое сообщение:')
+                #if event.to_me:
+                 #   print('Для меня от: ', event.user_id)
+                #else:
+                  #  print('От меня для: ', event.user_id)
+                #print('Текст:', event.text)
 
