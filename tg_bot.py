ad615a9108c205e29d564a02e9a8086c21a1097b
svgen83 <61458549+svgen83@users.noreply.github.com>
2022-07-12 23:26:41 +0400

    удалена функция detect intent texts

--- tg_bot.py
+++ tg_bot.py
@@ -23,15 +23,6 @@ def send_msg(update: Update, context: CallbackContext):
     msg = detect_intent_texts(
     project_id, update.message.chat_id, update.message.text, 'ru').query_result.fulfillment_text
     update.message.reply_text(msg)
-    
-    
-def detect_intent_texts(project_id, session_id, text, language_code):    
-  session_client = dialogflow.SessionsClient()
-  session = session_client.session_path(project_id, session_id)
-  text_input = dialogflow.TextInput(text=text, language_code=language_code)
-  query_input = dialogflow.QueryInput(text=text_input)
-  response = session_client.detect_intent(session=session, query_input=query_input)
-  return response.query_result.fulfillment_text
 
 
 def main():
